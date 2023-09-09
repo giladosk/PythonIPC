940590d19614fc9d2ab7a7a2f936b70ed2ca9493
giladosk <gilad@metomotion.com>
2023-09-09 17:53:44 +0300

    added shared memory

--- node/inter_process_node.py
+++ node/inter_process_node.py
@@ -5,15 +5,29 @@ from time import sleep
 
 
 class InterProcessNode:
-    def __init__(self, ipc_address, shared_mem_name, shared_mem_template=None):
+    def __init__(self, ipc_address, shared_mem_name, shared_mem_template):
         # initiate connections and shared memory blocks, node side
+        # define connection objects
         class ConnectionManager(BaseManager): pass
-
         ConnectionManager.register('get_lock')
         ConnectionManager.register("get_api")
         self.connection_manager = ConnectionManager(address=ipc_address, authkey=b'secret password')
 
+        # init shared memory
+        print('trying to connect to shared_memory')
+        while True:
+            try:
+                self.shm = shared_memory.SharedMemory(create=False, name=shared_mem_name, size=shared_mem_template.nbytes)
+
+                break
+            except FileNotFoundError:
+                continue
+        buffer = self.shm.buf
+        self.input_array = np.ndarray((200, 200), dtype=np.int64, buffer=buffer)
+        print(f'connected to shared memory, first argument is {self.input_array[0, 0]}')
+
     def start(self):
+        # TODO: arrange init<->start calls
         print('waiting for server')
         while True:
             try:
@@ -22,7 +36,7 @@ class InterProcessNode:
                 self._api = self.connection_manager.get_api()
                 print('connected to server')
                 break
-            except EOFError:
+            except (EOFError, ConnectionRefusedError):
                 continue
 
     def test_data(self):
@@ -31,14 +45,24 @@ class InterProcessNode:
         print(self._api.get_data())
 
     def test_lock(self):
-        # sleep(5)
         print('releasing lock')
         self._lock.release()
+
+    def test_shared_memory(self):
+        print(f'share memory has value of {self.input_array[0, 0]}')
+        self.input_array[0, 0] = 1
+        print(f'share memory has value of {self.input_array[0, 0]}')
 
 
 address = ('localhost', 6000)
 SHM_NAME = "NUMPY_SHMEM"
-node = InterProcessNode(ipc_address=address, shared_mem_name=SHM_NAME)
+array_template = np.ones(shape=(200, 200), dtype=np.int64)
+
+node = InterProcessNode(ipc_address=address, shared_mem_name=SHM_NAME, shared_mem_template=array_template)
 node.start()
 node.test_data()
 node.test_lock()
+node.test_shared_memory()
+print('shutting down')
+node.shm.close()
+node.shm.unlink()
--- server/inter_process_server.py
+++ server/inter_process_server.py
@@ -17,17 +17,23 @@ class InterProcessInterface:
 
 
 class InterProcessServer:
-    def __init__(self, ipc_address, shared_mem_name, shared_mem_template=None):
+    def __init__(self, ipc_address, shared_mem_name, shared_mem_template):
         # initiate connections and shared memory blocks, server side
+        # define connection objects
         class ConnectionManager(SyncManager): pass
         lock = Lock()
         data_share_api = InterProcessInterface()
         ConnectionManager.register('get_lock', callable=lambda: lock)
         ConnectionManager.register("get_api", callable=lambda: data_share_api)
-
         self.connection_manager = ConnectionManager(address=ipc_address, authkey=b'secret password')
 
+        # define shared memory
+        self.shm = shared_memory.SharedMemory(create=True, name=shared_mem_name, size=shared_mem_template.nbytes)
+        self.output_array = np.ndarray((200, 200), dtype=np.int64, buffer=self.shm.buf)
+        self.output_array.fill(-1)
+
     def start(self):
+        # TODO: arrange init<->start calls
         self.connection_manager.start()
         self._lock = self.connection_manager.get_lock()
         self._lock.acquire()
@@ -51,10 +57,31 @@ class InterProcessServer:
             released = self._lock.acquire(block=False)
         print('client connected to lock')
 
+    def test_shared_memory(self):
+        print('waiting for client to connect to shared memory')
+        # counter = 0
+        while self.output_array[0, 0] == -1:
+            # counter += 1
+            # if counter == 10000:
+            #     print(f'waiting. {self.output_array[0, 0]}')
+            #     counter = 0
+            continue
+        print(f'share memory has value of {self.output_array[0, 0]}')
+        print("shared_memory connected to by client")
+
+    def shutdown(self):
+        print('shutting down')
+        self.connection_manager.shutdown()
+        self.shm.close()
+        # self.shm.unlink()
+        # sleep(1)
+
 
 address = ('localhost', 6000)
 SHM_NAME = "NUMPY_SHMEM"
-server = InterProcessServer(ipc_address=address, shared_mem_name=SHM_NAME)
+array_template = np.ones(shape=(200, 200), dtype=np.int64)
+
+server = InterProcessServer(ipc_address=address, shared_mem_name=SHM_NAME, shared_mem_template=array_template)
 server.start()
 first_msg = server.test_data_alone()
 
@@ -68,3 +95,5 @@ while True:
         break
 
 server.test_lock()
+server.test_shared_memory()
+server.shutdown()
