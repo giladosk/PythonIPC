9f3337169d780a2af11f9ad9d4b11bef4c5b41c7
giladosk <gilad@metomotion.com>
2023-09-10 09:22:27 +0300

    re-arranged init of connection classes

--- node/inter_process_node.py
+++ node/inter_process_node.py
@@ -7,42 +7,45 @@ from time import sleep
 class InterProcessNode:
     def __init__(self, ipc_address, shared_mem_name, shared_mem_template):
         # initiate connections and shared memory blocks, node side
-        # define connection objects
+
+        # init connection objects
+        # declare custom manager references
         class ConnectionManager(BaseManager): pass
         ConnectionManager.register('get_lock')
         ConnectionManager.register("get_api")
         self.connection_manager = ConnectionManager(address=ipc_address, authkey=b'secret password')
-
-        # init shared memory
-        print('trying to connect to shared_memory')
-        while True:
-            try:
-                self.shm = shared_memory.SharedMemory(create=False, name=shared_mem_name, size=shared_mem_template.nbytes)
-
-                break
-            except FileNotFoundError:
-                continue
-        buffer = self.shm.buf
-        self.input_array = np.ndarray((200, 200), dtype=np.int64, buffer=buffer)
-        print(f'connected to shared memory, first argument is {self.input_array[0, 0]}')
-
-    def start(self):
-        # TODO: arrange init<->start calls
-        print('waiting for server')
+        # connect to the server and save references to its objects
+        print('connecting to server')
         while True:
             try:
                 self.connection_manager.connect()
                 self._lock = self.connection_manager.get_lock()
                 self._api = self.connection_manager.get_api()
-                print('connected to server')
                 break
             except (EOFError, ConnectionRefusedError):
                 continue
+        print('connected to server')
+
+        # init shared memory
+        print('connecting to shared_memory')
+        while True:
+            try:
+                self.shm = shared_memory.SharedMemory(create=False, name=shared_mem_name, size=shared_mem_template.nbytes)
+                buffer = self.shm.buf
+                self.input_array = np.ndarray((200, 200), dtype=np.int64, buffer=buffer)
+                break
+            except FileNotFoundError:
+                continue
+        print(f'connected to shared memory, first argument is {self.input_array[0, 0]}')
+
+    def start(self):
+        # TODO: arrange init<->start calls. "start" should be a thread that get requests and writes to a queue
+        pass
 
     def test_data(self):
-        print(self._api.get_data())
-        self._api.update_data(2, 7)
-        print(self._api.get_data())
+        print(self._api.get_data_object())
+        self._api.update_data_object(2, 7)
+        print(self._api.get_data_object())
 
     def test_lock(self):
         print('releasing lock')
--- server/inter_process_server.py
+++ server/inter_process_server.py
@@ -4,53 +4,59 @@ import numpy as np
 from time import sleep
 
 
-class InterProcessInterface:
+class DataExchangeAPI:
     def __init__(self):
         self.data = {'time': 0, 'value': None}
 
-    def update_data(self, new_time, new_value):
+    def update_data_object(self, new_time, new_value):
         self.data['time'] = new_time
         self.data['value'] = new_value
 
-    def get_data(self):
+    def get_data_object(self):
         return self.data
 
 
 class InterProcessServer:
     def __init__(self, ipc_address, shared_mem_name, shared_mem_template):
         # initiate connections and shared memory blocks, server side
-        # define connection objects
+
+        # init connection objects
+        # create custom manager with shared objects
         class ConnectionManager(SyncManager): pass
         lock = Lock()
-        data_share_api = InterProcessInterface()
         ConnectionManager.register('get_lock', callable=lambda: lock)
-        ConnectionManager.register("get_api", callable=lambda: data_share_api)
+        data_object_api = DataExchangeAPI()
+        ConnectionManager.register("get_api", callable=lambda: data_object_api)
         self.connection_manager = ConnectionManager(address=ipc_address, authkey=b'secret password')
-
-        # define shared memory
-        self.shm = shared_memory.SharedMemory(create=True, name=shared_mem_name, size=shared_mem_template.nbytes)
-        self.output_array = np.ndarray((200, 200), dtype=np.int64, buffer=self.shm.buf)
-        self.output_array.fill(-1)
-
-    def start(self):
-        # TODO: arrange init<->start calls
+        # start the manager and save references to its objects
+        print('creating connection server')
         self.connection_manager.start()
         self._lock = self.connection_manager.get_lock()
         self._lock.acquire()
         self._api = self.connection_manager.get_api()
 
+        # init shared memory
+        print('creating shared_memory')
+        self.shm = shared_memory.SharedMemory(create=True, name=shared_mem_name, size=shared_mem_template.nbytes)
+        self.output_array = np.ndarray((200, 200), dtype=np.int64, buffer=self.shm.buf)
+        self.output_array.fill(-1)
+
+    def start(self):
+        # TODO: arrange init<->start calls. "start" should be a thread that get requests and writes to a queue
+        pass
+
     def test_data_alone(self):
-        print(self._api.get_data())
-        self._api.update_data(1, 1)
-        first_msg = self._api.get_data()
+        print(self._api.get_data_object())
+        self._api.update_data_object(1, 1)
+        first_msg = self._api.get_data_object()
         print(first_msg)
         return first_msg
 
     def test_data_with_node(self):
-        return self._api.get_data()
+        return self._api.get_data_object()
 
     def test_lock(self):
-        print('lock created')
+        print('testing lock')
         released = self._lock.acquire(block=False)
         print(f'lock {released=}')
         while not released:
